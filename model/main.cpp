2447672ded646148f567d2501f15e6dae56f904a
Anton Kosterin <Antonyan.Kosterko@mail.ru>
2024-05-11 17:47:04 +0300

    add get_predict

---
+++ model/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+
+int main() {
+    system("python E:\\Courses\\BAUMAN\\sem2\\PKS\\practise\\The-dota2-predictor\\model\\get_predict.py");
+
+    return 0;
+}
